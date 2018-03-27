4b47a9388dc3bcd3b1c956f69393425c5fb7e6da
forte1st <37139145+forte1st@users.noreply.github.com>
2018-03-27 17:58:51 +0900

    Add files via upload

---
+++ VirtualXylophone/Mallet.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace VirsualXylophone {
+    public class Mallet : MonoBehaviour {
+
+        private Vector3 prevPos;
+        [System.NonSerialized]
+        public Vector3 velocity;
+
+        void FixedUpdate() {
+            velocity = (transform.position - prevPos) / Time.deltaTime;
+            prevPos = transform.position;
+        }
+    }
+}
